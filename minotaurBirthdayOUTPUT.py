c8cdcce01b79db4ea6e9c1098cd36fc91a2fb596
pwaller815 <143044827+pwaller815@users.noreply.github.com>
2024-02-22 22:31:54 -0500

    Add files via upload

---
+++ minotaurBirthdayOUTPUT.py
@@ -0,0 +1,17 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\19045\OneDrive\Desktop\python\minotaurBirthday.py
+How many guests are attending?
+5
+Thread #1 ate the cupcake
+The counter found an empty tray!
+Thread #4 ate the cupcake
+The counter found an empty tray!
+Thread #5 ate the cupcake
+The counter found an empty tray!
+Thread #3 ate the cupcake
+The counter found an empty tray!
+Thread #2 ate the cupcake
+The counter found an empty tray!
+All guests have entered the labyrinth.
---
+++ minotaurShowroomOUTPUT.py
@@ -0,0 +1,36 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\19045\OneDrive\Desktop\python\minotaurShowroom.py
+How many guests are attending?
+10
+Guest #1 just joined the queue.
+Guest #2 just joined the queue.
+Guest #3 just joined the queue.
+Guest #4 just joined the queue.
+Guest #5 just joined the queue.
+Guest #6 just joined the queue.
+Guest #7 just joined the queue.
+Guest #8 just joined the queue.
+Guest #9 just joined the queue.
+Guest #10 just joined the queue.
+Guest #1 entered the showroom.
+Guest #1 just joined the queue.
+Guest #2 entered the showroom.
+Guest #2 just joined the queue.
+Guest #3 entered the showroom.
+Guest #3 just joined the queue.
+Guest #4 entered the showroom.
+Guest #4 just joined the queue.
+Guest #5 entered the showroom.
+Guest #5 just joined the queue.
+Guest #6 entered the showroom.
+Guest #6 just joined the queue.
+Guest #7 entered the showroom.
+Guest #7 just joined the queue.
+Guest #8 entered the showroom.
+Guest #8 just joined the queue.
+Guest #9 entered the showroom.
+Guest #9 just joined the queue.
+Guest #10 entered the showroom.
+All guests have visited the showroom.
